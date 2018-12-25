509e8fca72f2fb53e9d0e7e7c04b491825bebf81
thejoulethief <anupamsanthosh@gmail.com>
2018-12-26 01:03:59 +0530

    got the map and form working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+
+import {
+  Card,
+  CardText,
+  Form,
+  Label,
+  FormGroup,
+  Input,
+  CardTitle,
+  Button
+} from "reactstrap";
+
+import "./App.css";
+
+const API = "https://ipapi.co/json";
 
 class App extends Component {
+  state = {
+    lat: 0,
+    lng: 0,
+    zoom: 2.3,
+    set: false
+  };
+
+  componentDidMount = () => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+          zoom: 13,
+          set: true
+        });
+      },
+      err => {
+        console.log("Request for location access rejected!");
+        console.log("Attempting to find approximate location by ip...");
+        fetch(API)
+          .then(response => response.json())
+          .then(location1 => {
+            console.log(
+              `Approximate coordinates: ${location1.latitude}, ${
+                location1.longitude
+              }`
+            );
+            this.setState({
+              lat: location1.latitude,
+              lng: location1.longitude,
+              zoom: 13,
+              set: true
+            });
+          });
+      }
+    );
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.message);
+  };
+
+  handleChange = event => {
+    console.log(event.target.value, event.target.id);
+    this.setState({ [event.target.id]: event.target.value });
+  };
+
   render() {
+    const position = [this.state.lat, this.state.lng];
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="map">
+        {" "}
+        <Map className="map" center={position} zoom={this.state.zoom}>
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {this.state.set ? (
+            <Marker position={position}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          ) : (
+            ""
+          )}
+        </Map>
+        <Card className="carder" body>
+          <CardTitle>Welcome!</CardTitle>
+          <CardText>Leave a pin telling me where you visited from!</CardText>
+
+          <Form onSubmit={this.handleSubmit}>
+            <FormGroup>
+              <Label for="name">Name</Label>
+              <Input
+                onChange={this.handleChange}
+                type="text"
+                id="name"
+                placeholder="Enter your name!"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="message">Message</Label>
+              <Input
+                onChange={this.handleChange}
+                type="textarea"
+                id="message"
+                placeholder="Enter your message!"
+              />
+            </FormGroup>
+            <Button color="info" type="submit">
+              Post!
+            </Button>
+          </Form>
+        </Card>
       </div>
     );
   }
